7bc4436163a2956df00095f15b1f1d6c00e8491f
unknown <darielaguilar2012@gmail.com>
2024-07-18 09:23:29 -0600

    [ADD] Added all project files, set cln cva tailwind-merge, initial style for button

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/ui/Button";
+import { FC } from 'react'
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <Button size='default'> Hello</Button>
+}
+
+export default page
